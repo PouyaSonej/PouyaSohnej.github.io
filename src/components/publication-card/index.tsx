43cbfa5dd3941a0e36d6e14018b90aab1e028576
Pouya Sohrabinejad <95095794+PouyaSonej@users.noreply.github.com>
2024-07-04 01:02:11 +0330

    Update index.tsx

--- src/components/publication-card/index.tsx
+++ src/components/publication-card/index.tsx
@@ -137,7 +137,7 @@ const PublicationCard = ({
                 </div>
                 <div className="col-span-2">
                   <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
-                    {loading ? renderSkeleton() : }
+                    {loading ? renderSkeleton() : renderSkeleton()}
                   </div>
                 </div>
               </div>
