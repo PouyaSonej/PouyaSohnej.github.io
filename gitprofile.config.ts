c09948dcc7b5bd871137e0cdbad1528ec4c4581a
Pouya Sohrabinejad <95095794+PouyaSonej@users.noreply.github.com>
2024-07-04 00:29:33 +0330

    Update gitprofile.config.ts

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -11,14 +11,12 @@ const CONFIG = {
    */
   base: '/',
   projects: {
+    description:
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nunc ut.',
+         
     github: {
       // display: true, // Display GitHub projects?
       header: 'Bio',
-      projects: [
-        {
-          // title: 'Project Name',
-          description:
-            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nunc ut.',},]
           // imageUrl:
             // 'https://img.freepik.com/free-vector/illustration-gallery-icon_53876-27002.jpg',
           // link: 'https://example.com',
